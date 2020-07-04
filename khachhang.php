1e06a633ea4db539d9e31290dc4957795260ec76
Inkesiter <truongduchainguyen@outlook.com>
2020-07-04 10:57:01 +0700

    thêm và chỉnh sửa các file có đuôi .php

--- khachhang.php
+++ khachhang.php
@@ -1,15 +1,15 @@
 <head>
     <h1 style="text-align: center;">MIDTERM WEB DEVELOPMENT</h1>
-    <h2 style="text-align: center;">BẢNG HÀNG HOÁ</h2>
+    <h2 style="text-align: center;">BẢNG KHÁCH HÀNG</h2>
 </head>
 
 <body>
     <table border="1">
         <tr>
-            <th>Mẫ hàng hoá</th>
-            <th>Tên hàng hoá</th>
-            <th>Đơn giá</th>
-            <th>Hình</th>
+            <th>Mã khách hàng</th>
+            <th>Họ tên</th>
+            <th>Điện thoại</th>
+            <th>Email</th>
         </tr>
         <?php
         $conn = mysqli_connect("localhost", "root", "", "hanghoa");
@@ -18,13 +18,13 @@
             die("Connection failed:".$conn -> connect_error);
         }
 
-        $sql='select MaHH, TenHH, DonGia, Hinh from hanghoa';
+        $sql='select MaKH, HoTen, DienThoai, Email from khachhang';
         $result = $conn -> query($sql);
 
         if($result -> num_rows > 0){
             while($row = $result->fetch_assoc()) {
-                    echo "<tr><td>".$row["MaHH"]."</td><td>".$row["TenHH"]."</td><td>".$row["DonGia"]."</td><td>".
-                    $row["Hinh"]."</td><td>";
+                    echo "<tr><td>".$row["MaKH"]."</td><td>".$row["HoTen"]."</td><td>".$row["DienThoai"]."</td><td>".
+                    $row["Email"]."</td><td>";
             }
             echo "</table>";
         }
